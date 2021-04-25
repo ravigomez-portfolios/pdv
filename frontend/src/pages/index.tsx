e21435dd1544e89aad853e4624f509139d5a1307
Rafael Gómez <ravigomez@gmail.com>
2021-04-26 00:25:33 +0200

    styling home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
+import { Container } from '../styles/pages/home'
+
 const Home: React.FC = () => {
-  return <div>Hello Next.js</div>
+  return (
+    <Container>
+      <div>Hello World !</div>
+    </Container>
+  )
 }
 export default Home
