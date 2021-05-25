44697060d657d8d297b80d9683efe9bfd5380d4d
Rafael Gómez <ravigomez@gmail.com>
2021-05-25 16:57:10 +0200

    Reclect data annotations from model to DTO

--- backend-dotnet/DTOs/ProductInsertDTO.cs
+++ backend-dotnet/DTOs/ProductInsertDTO.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace POS.Backend.DTOs{
   public class ProductInsertDTO{
+    [Required]
+    [MaxLength(250)]
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
